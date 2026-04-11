906566a24e40355e62e2973331c609e6ec1bfa5e
Raviprajapati07 <152835452+Ravindraprajapat@users.noreply.github.com>
2026-04-11 21:19:27 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ import useGetCurrentUser from './hooks/useGetCurrentUser'
 import UserDashBoard from './page/UserDashBoard'
 import useGetApplications from './hooks/useGetApplications'
 
-export const serverUrl ="https://aijobtrack-3.onrender.com"
+export const serverUrl ="https://aijobtrack-6.onrender.com"
 
 function App() {
 
